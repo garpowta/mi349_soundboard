060dc4ed58fae2ab9bc78e21e919dccd469d0021
Taylor <garpowta@msu.edu>
2021-06-17 16:06:43 -0400

    first attempt

---
+++ soundboard.js
@@ -0,0 +1,13 @@
+var buttonElement = document.getElementById('buttonone')
+var Audio = document.getElementById('baseballaudio')
+
+buttonElement.addEventListener('click', ()=>{
+    Audio.play();
+})
+
+var buttonElement = document.getElementById('buttontwo')
+var Audio = document.getElementById('bloopaudio')
+
+
+var buttonElement = document.getElementById('buttonthree')
+var Audio = document.getElementById('caraudio')
